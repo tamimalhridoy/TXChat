d5beba299171c9e7abb8b401446f428f3b73c09d
Tamim Al Hridoy <162357537+tamimalhridoy@users.noreply.github.com>
2025-10-30 12:32:45 +0600

    port

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(5000, () => console.log("Server is running on port 5000"));
+app.listen(4000, () => console.log("Server is running on port 4000"));
