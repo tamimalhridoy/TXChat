e4e39fd59a21a50354ce4f2e165fcc7d4c191406
Tamim Al Hridoy <162357537+tamimalhridoy@users.noreply.github.com>
2025-11-03 11:05:57 +0600

    start now

--- dbConfig/db.js
+++ dbConfig/db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConfig = () => {
   return mongoose
-    .connect(
-      "mongodb+srv://txchat:GE0eeBmsBfC4adUn@txchat0.ieqapcf.mongodb.net/txchat?retryWrites=true&w=majority&appName=txchat0"
-    )
+    .connect(process.env.DB_CONECTION_STRING)
     .then(() => console.log("Database Connected!"));
 };
 
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require("express");
-
 const dbConfig = require("./dbConfig/db");
-
 const app = express();
+require("dotenv").config();
 
 dbConfig();
 app.get("/", (req, res) => {
