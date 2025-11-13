e1ed08474b8875575e1735e50836caa999eec530
Tamim Al Hridoy <162357537+tamimalhridoy@users.noreply.github.com>
2025-11-13 22:32:31 +0600

    email

--- controller/authController.js
+++ controller/authController.js
@@ -1,10 +1,13 @@
 const userSchema = require("../models/userSchema");
 
-const registration = (req, res) => {
+const registration = async (req, res) => {
   const { fullName, email, password, avatar } = req.body;
-
   if (!fullName) return res.status(400).send("Name Is Requird!");
-  
+  if (!email) return res.status(400).send("Email Is Requird");
+  if (!password) return res.status(400).send("Password is required!");
+
+  const existingUser = await userSchema.findOne({ email });
+  if (!existingUser) return res.status(400).send("Email alredy exist");
 
   const User = new userSchema({
     fullName,
